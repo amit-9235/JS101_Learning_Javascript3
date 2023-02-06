cc86f4b173055d2adf992f91efc3f3fc72dee450
unknown <amit.ak4746@gmail.com>
2023-02-06 20:37:04 +0530

    login done

---
+++ iwcondionaljs/greater.js
@@ -0,0 +1,11 @@
+let a = 12;
+let b = 12;
+if(a>b){
+  console.log("Greater");
+}
+else if (a==b){
+  console.log("both equal");
+}
+else{
+  console.log("Smaller");
+}
---
+++ iwcondionaljs/password.js
@@ -0,0 +1,15 @@
+let userName = 457;
+let password = "qwerty";
+let inputUserName = 457
+let inputPassword = "werty";
+if(userName == inputUserName){
+  if(password == inputPassword){
+    console.log("user can login");
+  }
+  else{
+    console.log("incorrect password");
+  }
+}
+else{
+  console.log("user cannot login")
+}
